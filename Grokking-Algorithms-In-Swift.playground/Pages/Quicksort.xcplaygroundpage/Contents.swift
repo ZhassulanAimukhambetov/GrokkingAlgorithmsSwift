44be41b0b87ddb6290a2a2265305f5950fd6d3de
Zhassulan Aimukhambetov <57430894+ZhassulanAimukhambetov@users.noreply.github.com>
2020-04-07 00:27:53 +0600

    Update Contents.swift
    I think this func is better...)

--- Grokking-Algorithms-In-Swift.playground/Pages/Quicksort.xcplaygroundpage/Contents.swift
+++ Grokking-Algorithms-In-Swift.playground/Pages/Quicksort.xcplaygroundpage/Contents.swift
@@ -3,28 +3,11 @@
 import Foundation
 
 func quicksort(_ array: [Int]) -> [Int] {
-    if array.count < 2 {
+    if array.count > 0 {
         // Base case: arrays with 0 or 1 element are already “sorted.”
-        return array
+        return quicksort(array.droplast()) + array.last!
     } else {
-        // Recursive case
-        let pivot = array[0]
-        var less: [Int] = []
-        var greater: [Int] = []
-        // Sub-array of all the elements less than the pivot
-        for i in Array(array.dropFirst()) {
-            if i <= pivot {
-                less.append(i)
-            }
-        }
-        // Sub-array of all the elements greater than the pivot
-        for i in Array(array.dropFirst()) {
-            if i > pivot {
-                greater.append(i)
-            }
-        }
-        
-        return quicksort(less) + [pivot] + quicksort(greater)
+        return 0
     }
 }
 
